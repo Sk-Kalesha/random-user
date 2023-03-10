774206b6893a4937c251f7c5f15a897124b46867
Sk-Kalesha <kaleemuddeen.shaik@gmail.com>
2023-03-10 16:26:08 +0530

    Added Random-User

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// http module
+import {HttpClientModule} from '@angular/common/http';
+
+// toastr module
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+// fontawesome module
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,    
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
